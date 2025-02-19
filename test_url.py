ebaefc590968eb80989cc51441834b60b036dd4d
bdgillihan <34020757-bdgillihan@users.noreply.replit.com>
2025-02-19 01:51:04 +0000

    Restored to 'c5a6e73d7c720bbbb65422afb330c8a2f2fd0ff7'
    Replit-Restored-To:c5a6e73d7c720bbbb65422afb330c8a2f2fd0ff7

--- test_url.py
+++ test_url.py
@@ -1,21 +1,10 @@
-from utils import convert_url_to_dropbox_direct
+from routes import convert_audio_url
 
 # Test URLs
-gdrive_urls = [
-    "https://drive.google.com/file/d/1a2b3c4d5e6f7g8h9/view?usp=sharing",
-    "https://drive.google.com/open?id=1a2b3c4d5e6f7g8h9",
-]
-dropbox_urls = [
-    "https://www.dropbox.com/s/abcd1234/file.mp3?dl=0",
-    "https://www.dropbox.com/s/efgh5678/audio.mp3?dl=0&rlkey=xyz",
-]
+gdrive_url = "https://drive.google.com/file/d/1a2b3c4d5e6f7g8h9/view?usp=sharing"
+dropbox_url = "https://www.dropbox.com/s/abcd1234/file.mp3?dl=0"
 
-print("Testing Google Drive URLs:")
-for url in gdrive_urls:
-    print(f"Original: {url}")
-    print(f"Reformatted: {convert_url_to_dropbox_direct(url)}\n")
-
-print("Testing Dropbox URLs:")
-for url in dropbox_urls:
-    print(f"Original: {url}")
-    print(f"Reformatted: {convert_url_to_dropbox_direct(url)}\n")
+print("Original Google Drive URL:", gdrive_url)
+print("Reformatted Google Drive URL:", convert_audio_url(gdrive_url))
+print("\nOriginal Dropbox URL:", dropbox_url)
+print("Reformatted Dropbox URL:", convert_audio_url(dropbox_url))
